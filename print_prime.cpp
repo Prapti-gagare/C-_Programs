b6ea490a2455cd15826cadcb2b91b56813f8d255
Prapti-gagare <praptigagare11@gmail.com>
2026-01-14 13:46:16 +0530

    Print prime numbers upto n

---
+++ print_prime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if(num <= 1)
+        return false;
+    for(int i = 2; i * i <= num; i++) {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter value of n: ";
+    cin >> n;
+
+    cout << "Prime numbers up to " << n << " are:\n";
+    for(int i = 2; i <= n; i++) {
+        if(isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}

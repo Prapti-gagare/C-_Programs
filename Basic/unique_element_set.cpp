9eecebef6e9c860239b4c124dbd88f7c7518885a
Prapti-gagare <praptigagare11@gmail.com>
2026-03-22 13:43:32 +0530

    Folder

---
+++ Basic/unique_element_set.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() 
+{
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int n = 7;
+
+    set<int> s(arr, arr + n);
+
+    cout << "Unique elements count: " << s.size();
+
+    return 0;
+}

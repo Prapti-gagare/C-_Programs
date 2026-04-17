63979b754d24a7031c6f84b95b7c600d4ed03b1e
Prapti Gagare <praptigagare11@gmail.com>
2026-04-17 20:34:11 +0530

    Add files via upload

---
+++ LinkedList/DeleteNode_LL.cpp
@@ -0,0 +1,18 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode* node) 
+    {
+        node->val = node->next->val;
+        ListNode* temp = node->next;
+        node->next = node->next->next;
+        delete temp;
+    }
+};

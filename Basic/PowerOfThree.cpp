f654c0bec346e64186c2b997ce5d8904ec53e00f
Prapti Gagare <praptigagare11@gmail.com>
2026-06-20 09:22:01 +0530

    Add files via upload

---
+++ Basic/PowerOfThree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (sol.isPowerOfThree(n)) {
+        cout << n << " is a power of three." << endl;
+    } else {
+        cout << n << " is NOT a power of three." << endl;
+    }
+
+    return 0;
+}
